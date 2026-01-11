bf0c132b8ebc65a30df04add4186f834ff686979
Onjaniaina754 <o.rabenirina@fulldigits.com>
2026-01-11 17:57:40 +0300

    feat: header responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 /* ========== Navigation =========== */
+
+const hamburger = document.querySelector('.hamburger')
+const close = document.querySelector('.nav-list .close')
+hamburger.addEventListener('click', () => {
+    document.querySelector('.nav-list').classList.add('show')
+})
+
+close.addEventListener('click', () => {
+    document.querySelector('.nav-list').classList.remove('show')
+})
